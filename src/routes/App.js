f0e6f5d9a1e4f353086e60aacb19f6516f3769a1
John Pertuz <Johnpertuz@gmail.com>
2021-08-10 16:04:38 -0500

    #6391f7 Arreglando la funcionalidad del player

--- src/routes/App.js
+++ src/routes/App.js
@@ -5,11 +5,13 @@ import Login from "../container/Login";
 import NotFound from "../container/NotFound";
 import Register from "../container/Register";
 import Layout from "../components/Layout";
+import Player from "../components/Player";
 const App = () => (
   <BrowserRouter>
     <Layout>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/player/:id" component={Player} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
         <Route component={NotFound} />
